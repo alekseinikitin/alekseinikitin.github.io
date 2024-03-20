3a4d85cafd7956988e74b7715e7be94dd928cb51
alekseinikitin <161435260+alekseinikitin@users.noreply.github.com>
2024-03-21 00:20:40 +0300

    Add files via upload

---
+++ TaskJS/task6/task6.js
@@ -0,0 +1,28 @@
+// todo: Сохраните результат шахматной партии в виде двухмерного массива board, называть фигуры можно 
+// произвольно, например “black queen”.
+let position = ''
+let board = [
+    [{ name: 'w'}, { name: 'b'},{ name: 'w'},{ name: 'b'},{ name: 'w'},{ name: 'b'},{ name: 'w'},{ name: 'b'},],
+    [{ name: 'b'}, { name: 'w'},{ name: 'b'},{ name: 'w'},{ name: 'b'},{ name: 'w'},{ name: 'b'},{ name: 'w'},],
+    [{ name: 'w'}, { name: 'b'},{ name: 'w'},{ name: 'b'},{ name: 'w'},{ name: 'b'},{ name: 'w'},{ name: 'b'},],
+    [{ name: 'b'}, { name: 'w'},{ name: 'b'},{ name: 'w'},{ name: 'b'},{ name: 'w'},{ name: 'b'},{ name: 'w'},],
+    [{ name: 'w'}, { name: 'b'},{ name: 'w'},{ name: 'b'},{ name: 'w'},{ name: 'b'},{ name: 'w'},{ name: 'b'},],
+    [{ name: 'b'}, { name: 'w'},{ name: 'b'},{ name: 'w'},{ name: 'b'},{ name: 'w'},{ name: 'b'},{ name: 'w'},],
+    [{ name: 'w'}, { name: 'b'},{ name: 'w'},{ name: 'b'},{ name: 'w'},{ name: 'b'},{ name: 'w'},{ name: 'b'},],
+    [{ name: 'b'}, { name: 'w'},{ name: 'b'},{ name: 'w'},{ name: 'b'},{ name: 'w'},{ name: 'b'},{ name: 'w'},],
+]; 
+
+
+
+
+//     [{position:a8, name: 'w'}, {position:b8, name: 'b'},{position:c8, name: 'w'},{position:d8, name: 'b'},{position:e8, name: 'w'},{position:f8, name: 'b'},{position:g8, name: 'w'},{position:h8, name: 'b'},],
+//     [{position:a7, name: 'b'}, {position:b7, name: 'w'},{position:c7, name: 'b'},{position:d7, name: 'w'},{position:e7, name: 'b'},{position:f7, name: 'w'},{position:g7, name: 'b'},{position:h7, name: 'w'},],
+//     [{position:a6, name: 'w'}, {position:b6, name: 'b'},{position:c6, name: 'w'},{position:d6, name: 'b'},{position:e6, name: 'w'},{position:f6, name: 'b'},{position:g6, name: 'w'},{position:h6, name: 'b'},],
+//     [{position:a5, name: 'b'}, {position:b5, name: 'w'},{position:c5, name: 'b'},{position:d5, name: 'w'},{position:e5, name: 'b'},{position:f5, name: 'w'},{position:g5, name: 'b'},{position:h5, name: 'w'},],
+//     [{position:a4, name: 'w'}, {position:b4, name: 'b'},{position:c4, name: 'w'},{position:d4, name: 'b'},{position:e4, name: 'w'},{position:f4, name: 'b'},{position:g4, name: 'w'},{position:h4, name: 'b'},],
+//     [{position:a3, name: 'b'}, {position:b3, name: 'w'},{position:c3, name: 'b'},{position:d3, name: 'w'},{position:e3, name: 'b'},{position:f3, name: 'w'},{position:g3, name: 'b'},{position:h3, name: 'w'},],
+//     [{position:a2, name: 'w'}, {position:b2, name: 'b'},{position:c2, name: 'w'},{position:d2, name: 'b'},{position:e2, name: 'w'},{position:f2, name: 'b'},{position:g2, name: 'w'},{position:h2, name: 'b'},],
+//     [{position:a1, name: 'b'}, {position:b1, name: 'w'},{position:c1, name: 'b'},{position:d1, name: 'w'},{position:e1, name: 'b'},{position:f1, name: 'w'},{position:g1, name: 'b'},{position:h1, name: 'w'},],
+// ];
+console.log(board);
+alert (board)
